8be8dcd18698cd8786061930051bc0b01ab7a5e6
Orhan Cakmak <orhan.cakmak222@gmail.com>
2022-07-18 15:18:24 +0300

    classes was added

--- src/main/java/com/bootcamp/StudentManagementSystem/model/entity/Department.java
+++ src/main/java/com/bootcamp/StudentManagementSystem/model/entity/Department.java
@@ -1,4 +1,19 @@
 package com.bootcamp.StudentManagementSystem.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "department")
 public class Department {
+    private String name;
+    private List<Class> classes;
 }
--- src/main/java/com/bootcamp/StudentManagementSystem/model/entity/Faculty.java
+++ src/main/java/com/bootcamp/StudentManagementSystem/model/entity/Faculty.java
@@ -1,4 +1,20 @@
 package com.bootcamp.StudentManagementSystem.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "faculty")
 public class Faculty {
+    private String name;
+    private List<Department> departments;
+
 }

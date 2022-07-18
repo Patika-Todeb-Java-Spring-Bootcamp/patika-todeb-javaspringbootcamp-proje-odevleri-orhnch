04cf6c0af2ca58cae4d30f57a400149ee3ebbb3c
Orhan Cakmak <orhan.cakmak222@gmail.com>
2022-07-18 11:42:59 +0300

    entities were crated

---
+++ src/main/java/com/bootcamp/StudentManagementSystem/model/entity/Course.java
@@ -0,0 +1,31 @@
+package com.bootcamp.StudentManagementSystem.model.entity;
+
+import com.bootcamp.StudentManagementSystem.StudentManagementSystemApplication;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "course")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private int quota;
+    private int numberOfStudentChoseCourse;
+
+    @ManyToMany(mappedBy = "courses", cascade = CascadeType.MERGE)
+    private List<Student> students;
+
+
+
+}

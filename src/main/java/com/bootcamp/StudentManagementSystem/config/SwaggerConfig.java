b2e027b0dd198c35c4d5203474b6a56258e52c2e
Orhan Cakmak <orhan.cakmak222@gmail.com>
2022-08-07 06:06:57 +0300

    swagger authorizing was added

--- src/main/java/com/bootcamp/StudentManagementSystem/config/SwaggerConfig.java
+++ src/main/java/com/bootcamp/StudentManagementSystem/config/SwaggerConfig.java
@@ -2,33 +2,60 @@ package com.bootcamp.StudentManagementSystem.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
-    @Bean
-    public Docket customDocket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.bootcamp.StudentManagementSystem"))
-//                .paths(PathSelectors.regex("/api/.*"))
-                .build().apiInfo(apiInfo());
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", AUTHORIZATION_HEADER, "header");
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfo(
-                "Student Management System",
-                "Student Management System simulation",
-                "v1",
+                "Spring Boot Student Management Project",
+                "Spring Boot Blog REST API Documentation",
+                "1",
                 "Terms of service",
-                "orhancakmakeng@gmail.com",
+                new Contact("Orhan Cakmak", "", "orhancakmakeng@gmail.com"),
                 "License of API",
-                "xxxxx");
+                "API license URL",
+                Collections.emptyList()
+        );
     }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 
 }
